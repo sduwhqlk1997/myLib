12f49b17bc8678ac2fb918bc96132be2d729fe3d
root <root@LAPTOP-CTP3OMHS.localdomain>
2026-09-23 19:19:53 +0800

    switch DimScales to public variable type

--- include/SAW.hpp
+++ include/SAW.hpp
@@ -237,6 +237,21 @@ namespace SAW2_5D
         }
     };
     /**************************SAW器件序列************************************************* */
+    struct DimScales // 无量纲化特征量
+    {
+        double L0 = 1.0;     // 特征长度
+        double U0 = 1.0;     // 位移尺度
+        double c0 = 1.0;     // 刚度尺度
+        double e0 = 1.0;     // 压电系数尺度
+        double epcl0 = 1.0;  // 介电常数尺度
+        double rho0 = 1.0;   // 密度尺度
+        double omega0 = 1.0; // 频率尺度
+        double Phi0 = 1.0;   // 电势尺度
+        double sigma0 = 1.0; // 应力尺度
+        double D0 = 1.0;     // 电位移尺度
+        double v0 = 1.0;     // 波速尺度
+        double k0 = 1.0;     // 波数尺度
+    };
     class deviceArray
     {
         /*数据*/
@@ -263,21 +278,6 @@ namespace SAW2_5D
         int numSubStructures = 0;                       // 子结构种类数
 
     public:
-        struct DimScales // 无量纲化特征量
-        {
-            double L0 = 1.0;     // 特征长度
-            double U0 = 1.0;     // 位移尺度
-            double c0 = 1.0;     // 刚度尺度
-            double e0 = 1.0;     // 压电系数尺度
-            double epcl0 = 1.0;  // 介电常数尺度
-            double rho0 = 1.0;   // 密度尺度
-            double omega0 = 1.0; // 频率尺度
-            double Phi0 = 1.0;   // 电势尺度
-            double sigma0 = 1.0; // 应力尺度
-            double D0 = 1.0;     // 电位移尺度
-            double v0 = 1.0;     // 波速尺度
-            double k0 = 1.0;     // 波数尺度
-        };
         std::vector<SubProb> subProbs; // 子问题（用于区域分解），索引号表示子问题类型编号
         Mat_i baseStructureArray;      // 器件子结构序列，其中的数字表示子结构类型，当未调用区域分解函数时，数字与subStructures变量的索引对应；当调用过区域分解函数时，则数字对应于subProbs的索引
         Eigen::Matrix<geoDom, Eigen::Dynamic,
